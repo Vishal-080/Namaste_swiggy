ba325a7b7ffcb3e9a96c0eebe45a2efbe74a1e6e
Vishal Rathod <vrathod591@gmail.com>
2024-09-21 02:00:19 +0530

    Created body component

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,15 @@ const Header = ( ) => {
   )
 }
 
+const Body = () => {
+  return (
+    <div className='body'>
+      <div className='search'>Search bar</div>
+      <div className='res-container'></div>
+    </div>
+  )
+}
+
 const AppLayout = () => {
 
   return (
