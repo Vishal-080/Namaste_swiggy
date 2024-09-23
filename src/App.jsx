46fd4845df6b521e47e86ac878ce68d0e1734813
Vishal Rathod <vrathod591@gmail.com>
2024-09-23 19:16:20 +0530

    Order now button added

--- src/App.jsx
+++ src/App.jsx
@@ -19,13 +19,14 @@ const Header = ( ) => {
   )
 }
 
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
   return (
     <div className='res-card'>
       <img className='res-img' src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/xqwpuhgnsaf18te7zvtv"/>
-      <h2>Meghna Foods</h2>
-      <h3>Ratings</h3>
-
+      <h2>{props.resName}</h2>
+      <h4>{props.resRatings}</h4>
+      <h4>{props.price}</h4>
+      <button className='order-btn'>Order Now</button>
     </div>
   )
 }
@@ -37,16 +38,16 @@ const Body = () => {
         <input cl type="text" name="search" placeholder="Search Restaurants, Cusines, Dishes here..."/>
       </div>
       <div className='res-container'>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
+        <RestaurantCard resName={"Meghna Foods"} resRatings={"4.2 stars"} price={"200Rs for two"}/>
+        <RestaurantCard resName={"Vegily Foods"} resRatings={"4.2 stars"} price={"300Rs for two"}/>
+        <RestaurantCard resName={"Momosa"} resRatings={"4.8 stars"} price={"250Rs for two"}/>
+        <RestaurantCard resName={"Meghna Kitchen"} resRatings={"4.9 stars"} price={"100Rs for two"}/>
+        <RestaurantCard resName={"Meghna Foods"} resRatings={"4.2 stars"} price={"200Rs for two"}/>
+        <RestaurantCard resName={"Meghna Foods"} resRatings={"4.2 stars"} price={"200Rs for two"}/>
+        <RestaurantCard resName={"Meghna Foods"} resRatings={"4.2 stars"} price={"200Rs for two"}/>
+        <RestaurantCard resName={"Meghna Foods"} resRatings={"4.2 stars"} price={"200Rs for two"}/>
+        <RestaurantCard resName={"Meghna Foods"} resRatings={"4.2 stars"} price={"200Rs for two"}/>
+        <RestaurantCard resName={"Meghna Foods"} resRatings={"4.2 stars"} price={"200Rs for two"}/>
       </div>
     </div>
   )
