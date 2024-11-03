e416d4b2a5ba797cae5d9455ea222138392a4fde
Vishal Rathod <vrathod591@gmail.com>
2024-11-03 11:59:26 +0530

    Restaurant Card component added

---
+++ src/Components/RestaurantCard.js
@@ -0,0 +1,14 @@
+const RestaurantCard = (props) => {
+  return (
+    <div className="res-card">
+      <img
+        className="res-img"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/xqwpuhgnsaf18te7zvtv"
+      />
+      <h2>{props.resName}</h2>
+      <h4>{props.resRatings}</h4>
+      <h4>{props.price}</h4>
+      <button className="order-btn">Order Now</button>
+    </div>
+  );
+};
