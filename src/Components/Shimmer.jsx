aca157bef2256e3fd04cf0d092f6cd1bb25800db
Vishal Rathod <vrathod591@gmail.com>
2024-11-11 01:46:42 +0530

    Shimmer ui appied

--- src/Components/Shimmer.jsx
+++ src/Components/Shimmer.jsx
@@ -3,7 +3,19 @@ import React from "react";
 const Shimmer = () => {
   return (
     <div className="shimmer-container">
-      <h2>Loading ...! </h2>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
       <div className="shimmer-card"></div>
       <div className="shimmer-card"></div>
       <div className="shimmer-card"></div>
