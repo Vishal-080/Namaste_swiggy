2fd024598febfc1ed0e01284ba0bec52df3a98aa
Vishal Rathod <vrathod591@gmail.com>
2024-11-11 22:08:18 +0530

    Top rated restaurant filtered functionality implemented

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -4,43 +4,62 @@ import Shimmer from "./Shimmer";
 
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [filteredRestaurants, setFilteredRestaurant] = useState([]);
+  const [filteredList, setFilteredList] = useState(false);
 
   useEffect(() => {
     fetchData();
+    console.log("hello  ");
   }, []);
 
   const fetchData = async () => {
     const data = await fetch(
       "https://www.swiggy.com/mapi/restaurants/list/v5?offset=0&is-seo-homepage-enabled=true&lat=12.9352403&lng=77.624532&carousel=true&third_party_vendor=1"
     );
+
     const jsonData = await data.json();
 
     setListOfRestaurants(
-      jsonData?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+      jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+    setFilteredRestaurant(
+      jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
         ?.restaurants
     );
   };
 
-  if (listOfRestaurants.length === 0) {
-    return <Shimmer />;
-  }
+  const handleFilter = () => {
+   const filterRes =  listOfRestaurants.filter((res) => res?.info?.avgRating >= 4.3);
+   console.log(filterRes, "filterRes");
+   setFilteredRestaurant(filterRes);
+   setFilteredList(!false);
+  };
 
-  return (
+  return listOfRestaurants?.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="search">
         <input
           type="text"
           name="search"
+          className="search-box"
           placeholder="Search Restaurants, Cusines, Dishes here..."
         />
+        <button className="search-btn">🔍Search</button>
       </div>
       <div className="filter">
-        <button className="filter-btn">Top Rated Restaurants</button>
+        <button className="filter-btn" onClick={handleFilter}>
+          Top Rated Restaurants
+        </button>
       </div>
       <div className="res-container">
-        {listOfRestaurants.map((restaurant) => (
+        {filteredList ? (filteredRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant?.info?.id} resData={restaurant} />
-        ))}
+        ))) : (listOfRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant?.info?.id} resData={restaurant} />
+        )))}
       </div>
     </div>
   );
