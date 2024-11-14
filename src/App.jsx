04bf0a52daaa74365a7ba470dac7ea77b5f525ff
Vishal Rathod <vrathod591@gmail.com>
2024-11-15 01:39:31 +0530

    Routing config

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import "./App.css";
-import Header from './Components/Header';
+import Header from "./Components/Header";
 import Body from "./Components/Body";
-
+import About from "./Components/About";
+import { createBrowserRouter } from "react-router-dom";
 
 const AppLayout = () => {
   return (
@@ -11,5 +12,16 @@ const AppLayout = () => {
     </>
   );
 };
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+]);
 
 export default AppLayout;
