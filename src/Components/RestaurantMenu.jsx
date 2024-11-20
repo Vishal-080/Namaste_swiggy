8a73b9fa164117c26030061cb4d097dfc1c3ca14
Vishal Rathod <vrathod591@gmail.com>
2024-11-21 01:11:57 +0530

    Restaurnat dummy menu

--- src/Components/RestaurantMenu.jsx
+++ src/Components/RestaurantMenu.jsx
@@ -4,6 +4,13 @@ const RestaurantMenu = () => {
   return (
     <div className="menu">
       <div>RestaurantMenu</div>
+      <h2>Name of the restaurant</h2>
+      <h3>MENU</h3>
+      <ul>
+        <li>DOSA</li>
+        <li>SAMOSA</li>
+        <li>SANDWICH</li>
+      </ul>
     </div>
   );
 };
