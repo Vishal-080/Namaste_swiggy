71b252895fc4624264d537ee385be9f2ff542214
Vishal Rathod <vrathod591@gmail.com>
2024-11-20 11:08:31 +0530

    Restaurant menu component created

---
+++ src/Components/RestaurantMenu.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RestaurantMenu = () => {
+  return (
+    <div className="menu">
+      <div>RestaurantMenu</div>
+    </div>
+  );
+};
+
+export default RestaurantMenu;
