b039dcc88a7ffd4d363fe521e2a6bc00beb18b41
Vishal Rathod <vrathod591@gmail.com>
2024-12-05 02:39:02 +0530

    User Component

--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -4,7 +4,7 @@ const User = ({props}) => {
   return (
     <div className='user-card'>
         <h2>Name: {props.name}</h2>
-        <h3>Loaction: India</h3>
+        <h3>Loaction: {props.loc}</h3>
         <h4>Contact No:- 9998887776</h4>
     </div>
   )
