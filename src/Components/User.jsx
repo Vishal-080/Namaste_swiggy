e33ce40e059cbbc01f19e214920e8af9af0955cc
Vishal Rathod <vrathod591@gmail.com>
2024-12-05 02:32:04 +0530

    User Component

--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const User = () => {
   return (
-    <div>
-        
+    <div className='user-card'>
+        <h2>Name: Vishal</h2>
+        <h3>Loaction: India</h3>
+        <h4>Contact No:- 9998887776</h4>
     </div>
   )
 }
