f828c1e0c4a508fdd8047438596c4e1ac0f6a6de
Vishal Rathod <vrathod591@gmail.com>
2024-12-05 11:44:20 +0530

    About us page

--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
-const User = ({props}) => {
+const User = ({ props }) => {
   return (
-    <div className='user-card'>
-        <h2>Name: {props.name}</h2>
-        <h3>Loaction: {props.loc}</h3>
-        <h4>Contact No:- 9998887776</h4>
+    <div className="user-card">
+      <h2>Name: {props.name}</h2>
+      <h3>Loaction: {props.loc}</h3>
+      <h4>Contact No:- 9998887776</h4>
     </div>
-  )
-}
+  );
+};
 
 export default User;
