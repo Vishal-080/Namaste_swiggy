d947448ca8308e8b8d5685abefd52d0457ffaacc
Vishal Rathod <vrathod591@gmail.com>
2024-12-05 12:44:49 +0530

    Menu card

--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
-const User = ({ props }) => {
+const User = ({props}) => {
+
+
+  // const {userName} = props;
+
   return (
     <div className="user-card">
-      <h2>Name: {props.name}</h2>
-      <h3>Loaction: {props.loc}</h3>
+      <h2>Name: {props}</h2>
+      <h3>Loaction: {}</h3>
       <h4>Contact No:- 9998887776</h4>
     </div>
   );
