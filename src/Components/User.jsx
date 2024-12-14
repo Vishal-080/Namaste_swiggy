babd054ffc98f28e429fbef57b5a43cc5409a372
Vishal Rathod <vrathod591@gmail.com>
2024-12-14 21:05:35 +0530

    User profile

--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -8,7 +8,7 @@ const User = ({props}) => {
 
   return (
     <div className="user-card">
-      <h1>{user}</h1>
+      <h1>User :- {user}</h1>
       <h2>Name: {props}</h2>
       <h3>Location: {props}</h3>
       <h4>Contact No:-{props}</h4>
