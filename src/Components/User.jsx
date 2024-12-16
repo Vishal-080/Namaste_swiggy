bb9058bc8542af4c040f2fb2d2bda487f015f05e
Vishal Rathod <vrathod591@gmail.com>
2024-12-17 01:01:52 +0530

    user info

--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
 
 const User = (props) => {
+  const [userInfo, setUserInfo] = useState({
+    userName: "vishal",
+    loc: "mumbai",
+    num: "777888999",
+  });
 
-  const {userName, loc, num} = props;
+  const { userName, loc, num } = props;
 
   return (
     <div className="user-card">
-      <h2>Name:- {userName}</h2>
-      <h3>Location:- {loc}</h3>
-      <h4>Contact No:- {num}</h4>
+      <h2>Name:- {userInfo.userName}</h2>
+      <h3>Location:- {userInfo.loc}</h3>
+      <h4>Contact No:- {userInfo.num}</h4>
     </div>
   );
 };
