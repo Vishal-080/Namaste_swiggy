fe298d14849d6049894a54c91db56abd37ce9216
Vishal Rathod <vrathod591@gmail.com>
2024-12-16 14:19:27 +0530

    user count

--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 
 const User = ({ props }) => {
-  const [user, setUser] = useState("Vishal Rathod");
+  const [count, setCount] = useState(0);
   // const {userName} = props;
 
   return (
     <div className="user-card">
-      <h1>User :- {user}</h1>
+      <h1>Count :- {count}</h1>
+      <button onClick={()=> setCount(count+1)}>Increase Count</button>
       <h2>Name: {props}</h2>
       <h3>Location: {props}</h3>
       <h4>Contact No:-{props}</h4>
