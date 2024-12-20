46fc38e4aed0d156c90c6a86c29a1691cf5b9c90
Vishal Rathod <vrathod591@gmail.com>
2024-12-20 20:00:10 +0530

    res api

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -15,8 +15,8 @@ const Body = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      // "https://www.swiggy.com/mapi/restaurants/list/v5?&lat=12.9352403&lng=77.624532&"
       "https://www.swiggy.com/mapi/restaurants/list/v5?offset=0&is-seo-homepage-enabled=true&lat=12.9352403&lng=77.624532&carousel=true&third_party_vendor=1"
+      // "https://www.swiggy.com/mapi/restaurants/list/v5?&lat=12.9352403&lng=77.624532&"
     );
 
     const jsonData = await data.json();
