87f88ef66654ab9c6d8eaf4de4347ab10fb763d2
Vishal Rathod <vrathod591@gmail.com>
2024-12-25 00:30:02 +0530

    Res menu custom hook

--- src/utils/useRestaurantMenu.jsx
+++ src/utils/useRestaurantMenu.jsx
@@ -1,11 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const useRestaurantMenu = (resId) => {
 
   const [resInfo, setResInfo] = useState("");
   
+  useEffect(()=>{
+    fetchData();
+  },[]);
   
+  const fetchData = async () => {
+    const data = await fetch("")
+  };
+
+
   return (
+
   )
 }
 
