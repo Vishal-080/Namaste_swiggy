3f493be6f9a23526daa6f6431dcc7aeb7e5b1d5c
Vishal Rathod <vrathod591@gmail.com>
2024-12-29 00:19:38 +0530

    online status custom hook created

--- src/utils/useOnlineStatus.jsx
+++ src/utils/useOnlineStatus.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const useOnlineStatus = () => {
-  return (
-    <div>useOnlineStatus</div>
-  )
+
+    const [onlineStatus, setOnlineStatus] = useState(true);
+
+    useEffect(()=>{
+        window.addEventListener("offline", ()=>{
+            setOnlineStatus(false);
+        });
+
+        window.addEventListener("online", ()=>{
+            setOnlineStatus(true);
+        });
+
+    },[]);
+
+  return onlineStatus;
 }
 
 export default useOnlineStatus;
