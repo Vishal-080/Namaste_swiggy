268025a328427c582cf62c640c55144e44f302b7
Vishal Rathod <vrathod591@gmail.com>
2025-01-09 21:52:30 +0530

    Grocery component

--- src/Components/Grocery.jsx
+++ src/Components/Grocery.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Grocery = () => {
   return (
-    <div><h1>Our Grocery Store</h1></div>
-  )
-}
+    <div>
+      <h1>Our Grocery Store</h1>
+    </div>
+  );
+};
 
 export default Grocery;
