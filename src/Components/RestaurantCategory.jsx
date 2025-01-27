35a2a5f65f56204efeef8646acf0effb3732dd39
Vishal Rathod <vrathod591@gmail.com>
2025-01-27 13:41:12 +0530

    Res category component

--- src/Components/RestaurantCategory.jsx
+++ src/Components/RestaurantCategory.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React from "react";
 
-const RestaurantCategory = ({data}) => {
-
+const RestaurantCategory = ({ data }) => {
   // console.log(data[0]);
   return (
     <div>
-       <div className='border-b-2 m-4 p-2 flex justify-between items-center cursor-pointer'>{data.title} <span>🔽</span></div>
+      <div className="border-b-2 m-4 p-2 flex justify-between items-center cursor-pointer">
+        {data.title} <span>🔽</span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default RestaurantCategory;
