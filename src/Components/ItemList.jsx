c7b65e4767aac9d2799582faed81ce0ec595615a
Vishal Rathod <vrathod591@gmail.com>
2025-01-30 16:11:47 +0530

    item list css

--- src/Components/ItemList.jsx
+++ src/Components/ItemList.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ItemList = ({ items }) => {
-  console.log(items," item list")
+  console.log(items, " item list");
   return <div>{items?.card?.info?.name}Hello</div>;
 };
 
