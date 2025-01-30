c7b1f9192f5dbf08d601577b8832b7e291016278
Vishal Rathod <vrathod591@gmail.com>
2025-01-30 18:38:17 +0530

    item list rendering issue solved

--- src/Components/RestaurantCategory.jsx
+++ src/Components/RestaurantCategory.jsx
@@ -5,15 +5,17 @@ const RestaurantCategory = ({ data }) => {
   // console.log(data,"data   d");
   return (
     <div>
-      <div className="border-b-2 m-4 p-2 cursor-pointer">
-        <div className="m-2 p-1 text-left flex justify-between items-center">
-          <div className="font-semibold text-lg">
-            {data?.title} ({data?.itemCards?.length})
+      {data?.itemCards?.length ? (
+        <div className="border-b-2 m-4 p-2 cursor-pointer">
+          <div className="m-2 p-1 text-left flex justify-between items-center">
+            <div className="font-semibold text-lg">
+              {data?.title} ({data?.itemCards?.length})
+            </div>
+            <span>🔽</span>
           </div>
-          <span>🔽</span>
+          {data?.itemCards?.length ? <ItemList items={data?.itemCards} /> : ""}
         </div>
-        {data?.itemCards?.length ? <ItemList items={data?.itemCards} /> : "" }
-      </div>
+      ) : ( "" )}
     </div>
   );
 };
