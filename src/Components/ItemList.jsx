557172d88b6bce5d2aa1eefd0612799d092497b7
Vishal Rathod <vrathod591@gmail.com>
2025-02-01 01:12:07 +0530

    Item list

--- src/Components/ItemList.jsx
+++ src/Components/ItemList.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 
 const ItemList = ({ items }) => {
-  
-  console.log(items,"items");
+  console.log(items, "items");
 
   return (
-    <div >
+    <div>
       {items.map((item) => (
-        <div className="border-b-2 m-2 p-2 flex justify-between" key={item.card.info.id}>
+        <div
+          className="border-b-2 m-2 p-2 flex justify-between"
+          key={item.card.info.id}
+        >
           <p className="text-sm font-semibold">{item?.card?.info?.name}</p>
-          <span>Rs. {item?.card?.info?.price/100 || item?.card?.info?.defaultPrice/100
-          } ₹ </span>
+          <span>
+            Rs.{" "}
+            {item?.card?.info?.price / 100 ||
+              item?.card?.info?.defaultPrice / 100}{" "}
+            ₹{" "}
+          </span>
         </div>
       ))}
     </div>
