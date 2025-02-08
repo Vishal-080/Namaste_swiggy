5988a5134646669b743d91636214978518cbe263
Vishal Rathod <vrathod591@gmail.com>
2025-02-09 02:22:55 +0530

    UserContext used in App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import { useContext } from "react";
 import "./App.css";
 import Header from "./Components/Header";
 import { Outlet } from "react-router-dom";
+import UserContext from "./utils/UserContext";
 
 const AppLayout = () => {
+  const { loggedInUser } = useContext(UserContext);
+
   return (
     <>
-      <Header />
-      <Outlet />
+      <UserContext.Provider value={{ loggedInUser: "Vishal" }}>
+        <UserContext.Provider value={{ loggedInUser: "Vishal Rathod" }}>
+          <Header />
+        </UserContext.Provider>
+        <Outlet />
+      </UserContext.Provider>
     </>
   );
 };
