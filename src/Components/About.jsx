d1928c738650ce556203b8654ee14131f0caf780
Vishal Rathod <vrathod591@gmail.com>
2025-02-09 23:15:52 +0530

    About us page css replaced with tailwind

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -4,7 +4,7 @@ import User from "./User";
 const About = () => {
   return (
     <div className="text-center my-4 text-lg">
-      <h1>About us</h1>
+      <h1 className="text-xl font-bold">About us</h1>
       <User  />
     </div>
   );
