cce834213d30db64abf84f0028988f9374c18f70
Vishal Rathod <vrathod591@gmail.com>
2025-02-09 23:16:55 +0530

    Shimmer Ui text updated

--- src/Components/Shimmer.jsx
+++ src/Components/Shimmer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Shimmer = () => {
   return (
     <div className="m-5 p-2">
-      <p className="text-center font-bold text-xl">Loading...!!!<br/>Please refresh the page</p>
+      <p className="text-center font-bold text-xl">Loading...!!!<br/>or Please refresh the page</p>
       <div className="flex flex-wrap justify-center items-center my-12 mx-5">
         <div className="shimmer-card"></div>
         <div className="shimmer-card"></div>
