ba5159263779501273973c1cc8f13185ba514183
Vishal Rathod <vrathod591@gmail.com>
2025-02-12 23:56:39 +0530

    App store

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,22 @@ import "./App.css";
 import Header from "./Components/Header";
 import { Outlet } from "react-router-dom";
 import UserContext from "./utils/UserContext";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 
 const AppLayout = () => {
   const { loggedInUser } = useContext(UserContext);
 
   return (
     <>
+    <Provider store={appStore}>
       <UserContext.Provider value={{ loggedInUser: "Vishal" }}>
         <UserContext.Provider value={{ loggedInUser: "Vishal Rathod" }}>
           <Header />
         </UserContext.Provider>
         <Outlet />
       </UserContext.Provider>
+      </Provider>
     </>
   );
 };
