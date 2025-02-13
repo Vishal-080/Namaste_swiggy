6cd6e20c21f5f9ac8071a89c1dc1e297d52f7056
Vishal Rathod <vrathod591@gmail.com>
2025-02-13 23:08:58 +0530

    Cart slice code writting

--- src/utils/cartSlice.js
+++ src/utils/cartSlice.js
@@ -1,5 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const cartSlice = createSlice({
-
+    name:"cart",
+    initialState:{
+        items:[]
+    },
+    reducers:{
+        
+    }
 });
