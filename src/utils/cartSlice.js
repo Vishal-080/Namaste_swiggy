4806b35d27bb8c15d2b9394c7f6dc82cac6a1618
Vishal Rathod <vrathod591@gmail.com>
2025-02-16 23:52:08 +0530

    Dummy items from cart slice removed

--- src/utils/cartSlice.js
+++ src/utils/cartSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const cartSlice = createSlice({
   name: "cart",
   initialState: {
-    items: ["pizza", "Burger"],
+    items: [],
   },
   reducers: {
     addItems: (state, action) => {
