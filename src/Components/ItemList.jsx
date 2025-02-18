36074b46cf71674892688246c5fdba438ac8a356
Vishal Rathod <vrathod591@gmail.com>
2025-02-18 20:56:53 +0530

    Item list css

--- src/Components/ItemList.jsx
+++ src/Components/ItemList.jsx
@@ -40,7 +40,7 @@ const ItemList = ({ items }) => {
           <div className="w-3/12 flex flex-col justify-between items-center">
             <img src={RES_URL + item?.card?.info?.imageId} alt="res_img" />
             <button
-              className="rounded-lg bg-white text-black px-3 relative cursor-pointer text-lg font-medium"
+              className="rounded-lg bg-white text-black px-3 relative cursor-pointer text-lg font-medium my-1"
               onClick={() => handleAddItem(item)}
             >
               Add +
