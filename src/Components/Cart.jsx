ef14d9d005f44c51ab5dba7e78d8e535719af5da
Vishal Rathod <vrathod591@gmail.com>
2025-02-18 20:32:02 +0530

    Item list imported in cart component

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import ItemList from './ItemList';
+import { useSelector } from 'react-redux';
 
 const Cart = () => {
+
+    const cartItems = useSelector((store) => store.cart.items);
+
   return (
-    <div className='font-bold text-2xl text-center'>Our Cart Page</div>
+    <div className='font-bold text-2xl text-center w-6/12 m-auto'>
+        <h1>My Cart</h1>
+        <ItemList items={cartItems}></ItemList>
+    </div>
   )
 }
 
