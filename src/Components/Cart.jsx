580482d373539059b7230ec669042dcff607dc1d
Vishal Rathod <vrathod591@gmail.com>
2025-02-19 01:55:49 +0530

    Cart page clear cart feature added

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import ItemList from "./ItemList";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { clearCart } from "../utils/cartSlice";
 
 const Cart = () => {
 
   const cartItems = useSelector((store) => store.cart.items);
 
+  const dispatch = useDispatch();
+
+  const handleClearCart = () => {
+    dispatch(clearCart());
+  }
+
   return (
     <div className="text-center w-6/12 m-auto">
       <h1 className="font-semibold text-2xl">My Cart</h1>
+      <button className="px-4 py-2 text-center m-2 rounded-lg bg-black text-white cursor-pointer" onClick={handleClearCart}>Clear Cart</button>
       <ItemList items={cartItems}></ItemList>
     </div>
   );
