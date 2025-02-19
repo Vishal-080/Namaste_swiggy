daf212698dd70ac34293822235606efb369c6c46
Vishal Rathod <vrathod591@gmail.com>
2025-02-19 13:29:26 +0530

    Grocery page css

--- src/Components/Grocery.jsx
+++ src/Components/Grocery.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Grocery = () => {
   return (
-    <div>
+    <div className="text-center m-4 font-semibold text-xl">
       <h1>Grocery Store</h1>
     </div>
   );
