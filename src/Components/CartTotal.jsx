5596bbc4388ed17c471b1d05f43e27e9e26bf957
Vishal Rathod <vrathod591@gmail.com>
2025-02-25 17:51:09 +0530

    Pay button added

--- src/Components/CartTotal.jsx
+++ src/Components/CartTotal.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React from "react";
 
-const CartTotal = ({total}) => {
+const CartTotal = ({ total }) => {
   return (
     <>
-    <div className='border-t-2 border-gray-300 mt-8 p-2 font-semibold text-xl text-center flex justify-between items-center'>
-        <h1 className='mt-4'>Total amount :-</h1>
-        <p className='mt-4'>Rs. {total} ₹</p>
-    </div>
-    <button className="px-4 py-2 rounded-lg bg-green-400 text-white font-medium cursor-pointer">Pay {total}</button>
+      <div className="border-t-2 border-gray-300 mt-8 p-2 font-semibold text-xl text-center flex justify-between items-center">
+        <h1 className="mt-4">Total amount :-</h1>
+        <p className="mt-4">Rs. {total} ₹</p>
+      </div>
+      <button className="px-4 py-2 rounded-lg bg-green-400 text-white font-medium cursor-pointer">
+        Pay {total}
+      </button>
     </>
-  )
-}
+  );
+};
 
 export default CartTotal;
