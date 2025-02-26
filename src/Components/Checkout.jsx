0882ac78a46700acc0c41866e5f22db796cc0c05
Vishal Rathod <vrathod591@gmail.com>
2025-02-26 19:21:02 +0530

    Checkout page code writting

--- src/Components/Checkout.jsx
+++ src/Components/Checkout.jsx
@@ -2,16 +2,22 @@ import React from "react";
 
 const Checkout = () => {
   return (
-    <div >
-        <h1 className="font-bold text-xl m-2 text-center">Check-Out</h1>
+    <div>
+      <h1 className="font-bold text-xl m-2 text-center">Check-Out</h1>
       <div className="flex flex-col justify-between items-center my-2 py-6 border text-left">
         <h1 className="font-bold text-lg mb-4 text-center">Address details</h1>
-        <label htmlFor="house-no">House no./Street name :-  <input type="text" name="house-no"/></label>
-        <br/>
-        <label htmlFor="address">Address  :-  <input type="text" name="address" /></label>
-        <br/>
-        <label htmlFor="city">City  :-  <input type="text" name="city" /></label>
-        <br/>
+        <label htmlFor="house-no">
+          House no./Street name :- <input type="text" name="house-no" />
+        </label>
+        <br />
+        <label htmlFor="address">
+          Address :- <input type="text" name="address" />
+        </label>
+        <br />
+        <label htmlFor="city">
+          City :- <input type="text" name="city" />
+        </label>
+        <br />
         <button className="">Confirm Address</button>
       </div>
       <div>
