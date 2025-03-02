4c716965d59a7b0d45111e2677cda4770e2aa770
Vishal Rathod <vrathod591@gmail.com>
2025-03-02 12:15:54 +0530

    Login page coding

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+        <fieldset className="fieldset w-xs bg-base-200 border border-base-300 p-4 rounded-box">
+  <legend className="fieldset-legend">Login</legend>
+  
+  <label className="fieldset-label">Email</label>
+  <input type="email" className="input" placeholder="Email" />
+  
+  <label className="fieldset-label">Password</label>
+  <input type="password" className="input" placeholder="Password" />
+  
+  <button className="btn btn-neutral mt-4">Login</button>
+</fieldset>
+    </div>
   )
 }
 
