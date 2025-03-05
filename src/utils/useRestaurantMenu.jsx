5fedeed04df41e188621f995f0b3f2ba64c99a37
Vishal Rathod <vrathod591@gmail.com>
2025-03-06 00:11:41 +0530

    restaurant menu hook updated

--- src/utils/useRestaurantMenu.jsx
+++ src/utils/useRestaurantMenu.jsx
@@ -14,7 +14,6 @@ const useRestaurantMenu = (resId) => {
     const jsonData = await data.json();
     setResInfo(jsonData.data);
 
-    // console.log("jsonData resinfo", jsonData);
   };
 
   return resInfo;
